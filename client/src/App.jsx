f26bbfb99258f27785fb5540f07117b5d5c9b525
Abhideep Maity <abhideepm@gmail.com>
2020-08-25 19:48:09 +0530

    add video source

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import io from 'socket.io-client'
 import Peer from 'peerjs'
 const ENDPOINT = `http://127.0.0.1:5000/`
 
 const App = () => {
 	const [response, setResponse] = useState('')
+	const videoRef = useRef()
 
 	useEffect(() => {
 		const myPeer = new Peer({
@@ -20,8 +21,21 @@ const App = () => {
 		myPeer.on('open', id => {
 			socket.emit('join-room', id)
 		})
+		navigator.mediaDevices
+			.getUserMedia({ video: true, audio: true })
+			.then(stream => {
+				videoRef.current.srcObject = stream
+				videoRef.current.addEventListener('loadedmetadata', () => {
+					videoRef.current.play()
+				})
+			})
 	}, [])
-	return <div>{response}</div>
+	return (
+		<div>
+			{response}
+			<video muted ref={videoRef}></video>
+		</div>
+	)
 }
 
 export default App
