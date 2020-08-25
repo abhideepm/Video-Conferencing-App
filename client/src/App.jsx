c5c77c7f0c6c922436b7a1040ec3b38082ee14d1
Abhideep Maity <abhideepm@gmail.com>
2020-08-25 19:26:54 +0530

    add peerjs and socket.io

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import io from 'socket.io-client'
+import Peer from 'peerjs'
+const ENDPOINT = `http://127.0.0.1:5000/`
 
 const App = () => {
-	return <div></div>
+	const [response, setResponse] = useState('')
+
+	useEffect(() => {
+		const myPeer = new Peer({
+			host: '/',
+			port: '5001',
+		})
+		const socket = io(ENDPOINT)
+
+		socket.on('Hello', data => {
+			setResponse(data)
+		})
+
+		myPeer.on('open', id => {
+			socket.emit('join-room', id)
+		})
+	}, [])
+	return <div>{response}</div>
 }
 
 export default App
